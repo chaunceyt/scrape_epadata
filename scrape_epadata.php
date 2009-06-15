26c97905519f369a3e11dd6a18223579e8921d37
Chauncey Thorn <chaunceyt@gmail.com>
2009-06-14 22:27:58 -0400

    removed create tbl code

--- scrape_epadata.php
+++ scrape_epadata.php
@@ -81,76 +81,8 @@ foreach($files as $file) {
 
     $filedata = file($file);
     
-    //TODO: fix table create code - buggy (100% broken)
-    //detect the file type
-    if(preg_match('/_1_/',$file)) {
-        //File Type 1: Facility, Chemical, Releases and Other Waste Management Summary Information
-        $file_type = _type1;
-    }
-    else if(preg_match('/_2a_/',$file)) {
-        //File Type 2A: Detailed Source Reduction Activities and Methods
-        $file_type = _type2a;
-    }
-    else if(preg_match('/_2b_/',$file)) {
-        //File Type 2B: Detailed On-Site Waste Treatment Methods and Efficiency
-        $file_type = _type2b;
-    }
+    $state_str = $_state[0];
 
-    else if(preg_match('/_3a_/',$file)) {
-        //File Type 3A: Details of Transfers Off-site
-        $file_type = _type3a;
-    }
-
-    else if(preg_match('/_3b_/',$file)) {
-        //File Type 3B: Details of Transfers to Publicly Owned Treatment Works (POTW)
-        $file_type = _type3b;
-    }
-
-    else if(preg_match('/_4_/',$file)) {
-        //File Type 4: Details of Facility Information
-        $file_type = _type4;
-    }
-
-    else if(preg_match('/_5_/',$file)) {
-        //File Type 5: Additional Information on Source Reduction, Recycling and Pollution Control
-        $file_type = _type5;
-    }
-
-    $fieldnames = array_shift($filedata);
-    $fields = explode("\t",$fieldnames);
-
-    //print_r($fields);
-    foreach($fields as $fieldname) {
-        $tablefields[] = string_to_filename($fieldname);
-    }
-
-    //print_r($tablefields);
-    $end = end($tablefields);
-    
-    //TODO: fix - broken
-    //echo "Creating SQL Create script for file: " . $file ."...\n";
-    //echo "TABLENAME: epa_data".$file_type."\n";
-    /*
-    $str = "CREATE TABLE epa_data".$file_type."  ("."\n";
-    foreach($tablefields as $field) {
-
-        if($field == $end) {
-            $str .= $field . " varchar(200) NOT NULL DEFAULT ''"."\n";
-        }
-        else {
-            $str .= $field . " varchar(200) NOT NULL DEFAULT '',"."\n";
-        }
-    }   
-
-    $str .= ")";
-    $_createtables++;
-    $fp = fopen("epa_data".$file_type.".sql", "w");
-    fwrite($fp, $str);
-    fclose($fp);
-    */
-    //echo $str;
-    $state_str = $_state[0];   
-    //echo "Creating csv file: " . $state_str."_".$year.$file_type.".csv";
     $open = fopen($state_str."_".$year.$file_type.".csv", "a");
     foreach($filedata as $data) {
         $data = explode("\t", trim($data));
